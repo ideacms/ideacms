959f9e0714ca9067d6d969a6baebc3397c38fe68
Morgan <m.delavenne@altimus360.com>
2018-01-09 14:48:24 +0100

    database

--- config/database.php
+++ config/database.php
@@ -2,28 +2,41 @@
 // les drivers de db
 // - mysql
 // - sqlite
-// - redis
+// - redis ?
+// - lowdb
 
 // le tableau avec la liste des connexions
 // le versionnement de la base par git (gestion par fichier dans le dossier database)
 return [
 	/*
-    | Database Connection
+    | Database driver
     */
-	'connection' => [
-		'driver' => 'mysql',
-		'host' => env('DB_HOST', '127.0.0.1'),
-		'port' => env('DB_PORT', '3306'),
-		'database' => env('DB_DATABASE', 'forge'),
-		'username' => env('DB_USERNAME', 'forge'),
-		'password' => env('DB_PASSWORD', ''),
-		'unix_socket' => env('DB_SOCKET', ''),
-		'charset' => 'utf8mb4',
-		'collation' => 'utf8mb4_unicode_ci',
-		'prefix' => '',
-		'strict' => true,
-		'engine' => 'InnoBD',
-	],
+	'driver' => 'mysql',
+
+	/*
+	| Driver parameters
+	*/
+	'host' => env('DB_HOST', '127.0.0.1'),
+
+	'port' => env('DB_PORT', '3306'),
+
+	'database' => env('DB_DATABASE', 'forge'),
+
+	'username' => env('DB_USERNAME', 'forge'),
+
+	'password' => env('DB_PASSWORD', ''),
+
+	'unix_socket' => env('DB_SOCKET', ''),
+
+	'charset' => 'utf8mb4',
+
+	'collation' => 'utf8mb4_unicode_ci',
+
+	'prefix' => '',
+
+	'strict' => true,
+
+	'engine' => 'InnoBD',
 
 	/*
     | Versioning
@@ -33,13 +46,11 @@ return [
 
 /* Exemple de connection
 
-'connection' => [
 	'driver' => 'sqlite',
 	'database' => env('DB_DATABASE', database_path('database.sqlite')),
 	'prefix' => '',
-],
 
-'connection' => [
+
 	'driver' => 'mysql',
 	'host' => env('DB_HOST', '127.0.0.1'),
 	'port' => env('DB_PORT', '3306'),
@@ -52,9 +63,8 @@ return [
 	'prefix' => '',
 	'strict' => true,
 	'engine' => null,
-],
 
-'connection' => [
+
 	'driver' => 'pgsql',
 	'host' => env('DB_HOST', '127.0.0.1'),
 	'port' => env('DB_PORT', '5432'),
@@ -65,12 +75,11 @@ return [
 	'prefix' => '',
 	'schema' => 'public',
 	'sslmode' => 'prefer',
-],
 
-'connection' => [
+
 	'driver' => 'lowdb',
 	'database' => env('DB_DATABASE', database_path('database.sqlite')),
 	'prefix' => '',
-],
+
 
 */
